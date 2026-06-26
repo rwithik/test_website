a3031544c570b99d2a84c2790c58d5e0a226c5f0
Manoj G <manojg@Manojs-MacBook-Air.local>
2026-06-26 22:19:22 +0530

    Updasted the admin dassh board

--- src/app/admin/popup/page.tsx
+++ src/app/admin/popup/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-export default function PopupManagement() {
-  return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold">Popup Management</h1>
-      
-    </div>
-  );
+export default function Page() {
+  return <div>Coming Soon</div>;
 }
